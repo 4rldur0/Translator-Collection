58682c34c83b754b86fbfb06027098129a326e7f
4rldur0 <ngim24863@gmail.com>
2024-05-02 01:04:26 +0900

    finish Connecting to the Backend

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 
 @app.get("/api/v1/hello")
-async def root():
+def root():
     return {"message": "Hello World"}
+
+app.mount("/", StaticFiles(directory="ui/dist", html=True), name="ui")
