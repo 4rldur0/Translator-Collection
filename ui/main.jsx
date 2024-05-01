ee891c3c669a4a36cbd379184da447b677d0e2e0
4rldur0 <ngim24863@gmail.com>
2024-05-02 04:39:56 +0900

    finish Creating an Index

--- ui/main.jsx
+++ ui/main.jsx
@@ -3,9 +3,33 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 async function main() {
+  const filmsResponse = await fetch("/api/v1/films");
+  const films = await filmsResponse.json();
+
   const rootElt = document.getElementById("app");
   const root = createRoot(rootElt);
-  root.render(<App />);
+  root.render(
+    films.map((film) => (
+      <ul>
+        <li>
+          <FilmEntry
+            id={film.id}
+            title={film.title}
+            description={film.description}
+          />
+        </li>
+      </ul>
+    )),
+  );
+}
+
+
+function FilmEntry({ id, title, description }) {
+  return (
+    <p>
+      <a href={`/film/${id}`}>{title}</a>: {description}
+    </p>
+  );
 }
 
 window.onload = main;
